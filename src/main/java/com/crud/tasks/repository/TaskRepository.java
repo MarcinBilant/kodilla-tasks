2a04e1d8a3b8fce30e5497554ae60373ef144509
Marcin Bilant <marcinb@fabelo.local>
2019-08-26 19:35:10 +0200

    19.2. Tworzenie połączenia z bazą danych z poziomu aplikacji -wersja01

---
+++ src/main/java/com/crud/tasks/repository/TaskRepository.java
@@ -0,0 +1,17 @@
+package com.crud.tasks.repository;
+
+import com.crud.tasks.domain.Task;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+
+public interface TaskRepository extends CrudRepository<Task, Long> {
+    @Override
+    List<Task> findAll();
+
+    @Query
+    List<Task> retrieveTasks(@Param("ID") int id);
+
+}
